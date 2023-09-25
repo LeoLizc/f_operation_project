18d66afaf8d4e60d7b2255a88537258dbc06e8ea
LeoLizc <ldlizcano@uninorte.edu.co>
2023-09-24 20:04:45 -0500

    optional filters

--- lib/domain/repositories/session_repository.dart
+++ lib/domain/repositories/session_repository.dart
@@ -1,7 +1,7 @@
 import 'package:f_operation_project/domain/models/session.dart';
 
 abstract class SessionRepository {
-  Future<GameSession> getSession({String id, String username});
+  Future<GameSession> getSession({String? id, String? username});
   Future<bool> addSession(GameSession session);
   // Future<bool> updateSession(GameSession session);
   Future<bool> deleteSession(GameSession session);
--- lib/domain/repositories/user_repository.dart
+++ lib/domain/repositories/user_repository.dart
@@ -1,7 +1,7 @@
 import 'package:f_operation_project/domain/models/user.dart';
 
 abstract class UserRepository {
-  Future<User> getUser({String id, String username});
+  Future<User> getUser({String? id, String? username});
   // Future<List<User>> getUsers();
   Future<bool> addUser(User user);
   Future<bool> updateUser(User user);
