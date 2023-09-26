28d76c41fea16e480b993acaa55ba98f8409473f
LeoLizc <ldlizcano@uninorte.edu.co>
2023-09-26 18:36:01 -0500

    game_controller updated

--- lib/ui/controllers/game_controller.dart
+++ lib/ui/controllers/game_controller.dart
@@ -1,17 +1,14 @@
 import 'package:f_operation_project/domain/models/operation.dart';
+import 'package:f_operation_project/domain/use_cases/game_usecases.dart';
 import 'package:get/get.dart';
 
 class GameController extends GetxController {
-  final RxList<Operation> _operations = <Operation>[
-    Operation(operando1: 1, operando2: 1, operador: '+', resultado: 2),
-    Operation(operando1: 1, operando2: 2, operador: '+', resultado: 3),
-    Operation(operando1: 2, operando2: 1, operador: '+', resultado: 3),
-    Operation(operando1: 2, operando2: 2, operador: '+', resultado: 4),
-    Operation(operando1: 3, operando2: 1, operador: '+', resultado: 4),
-    Operation(operando1: 3, operando2: 2, operador: '+', resultado: 5),
-  ].obs;
+  final GameUseCase _gameUseCase = Get.find<GameUseCase>();
+
+  final RxList<Operation> _operations = <Operation>[].obs;
   final RxInt _currentOperationIndex = 0.obs;
   int _score = 0;
+  int level = 1;
 
   Operation? get operation {
     if (_currentOperationIndex.value >= _operations.length) {
@@ -27,19 +24,12 @@ class GameController extends GetxController {
     _operations.clear();
     _currentOperationIndex.value = 0;
     _score = 0;
-    // TODO: HERE USE THE USECASES
-    _operations.assignAll([
-      Operation(operando1: 1, operando2: 1, operador: '+', resultado: 2),
-      Operation(operando1: 1, operando2: 2, operador: '+', resultado: 3),
-      Operation(operando1: 2, operando2: 1, operador: '+', resultado: 3),
-      Operation(operando1: 2, operando2: 2, operador: '+', resultado: 4),
-      Operation(operando1: 3, operando2: 1, operador: '+', resultado: 4),
-      Operation(operando1: 3, operando2: 2, operador: '+', resultado: 5),
-    ]);
+
+    _operations.assignAll(_gameUseCase.startGame(level));
   }
 
   bool answer(int answer) {
-    if (operation != null && operation?.resultado == answer) {
+    if (operation != null && _gameUseCase.checkAnswer(operation!, answer)) {
       _score++;
     }
 
