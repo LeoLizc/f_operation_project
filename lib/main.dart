fbe3b2d2e315a65ab67b307bd53b4ca1192b49ef
LeoLizc <ldlizcano@uninorte.edu.co>
2023-09-26 21:34:28 -0500

    adding the dependency injections

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
-import 'package:f_operation_project/data/repositories/auth/mock_auth_repository.dart';
+import 'package:f_operation_project/data/repositories/auth/memory_auth_repository.dart';
+import 'package:f_operation_project/data/repositories/session/memory_session_repo.dart';
+import 'package:f_operation_project/data/repositories/user/memory_user_repo.dart';
 import 'package:f_operation_project/domain/repositories/auth_repositoy.dart';
+import 'package:f_operation_project/domain/repositories/session_repository.dart';
+import 'package:f_operation_project/domain/repositories/user_repository.dart';
 import 'package:f_operation_project/domain/use_cases/authentication_usecase.dart';
+import 'package:f_operation_project/domain/use_cases/game_usecases.dart';
 import 'package:f_operation_project/ui/controllers/auth_controller.dart';
 import 'package:f_operation_project/ui/controllers/game_controller.dart';
 import 'package:f_operation_project/ui/pages/activity.dart';
+import 'package:f_operation_project/ui/pages/authentication/registro.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'ui/pages/authentication/login.dart';
 
 void main() {
-  Get.put<AuthRepository>(MockAuthRepository());
+  Get.put<UserRepository>(MemoryUserRepository());
+  Get.put<SessionRepository>(MemorySessionRepository());
+  Get.put<AuthRepository>(MemoryAuthRepository());
+  Get.put(GameUseCase());
   Get.put(AuthenticationUseCase());
   Get.put(GameController());
   Get.put(AuthController());
@@ -39,7 +48,11 @@ class MyApp extends StatelessWidget {
                   ? const ActivityPage()
                   : const Login());
             }),
-        // GetPage(name: '/activity', page: () => const Activity()),
+        GetPage(
+            name: '/register',
+            page: () => const Registro(
+                  key: Key('Registro'),
+                ))
       ],
     );
   }
