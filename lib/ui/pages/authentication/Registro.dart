049594a7707f6ec4b39c1e8f58fd957ce0e52e0e
LeoLizc <ldlizcano@uninorte.edu.co>
2023-09-26 22:55:36 -0500

    Register page now accept first and last name

--- lib/ui/pages/authentication/Registro.dart
+++ lib/ui/pages/authentication/Registro.dart
@@ -35,193 +35,249 @@ class RegistroP extends State<Registro> {
           padding: const EdgeInsets.all(20.0),
           child: Form(
             key: _formulario,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //campo en el cual el usuario va a ingresar su usuario
-                const Text("Ingrese su usuario",
-                    style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  key: const ValueKey("UsuarioRegistro"),
-                  controller: _user,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Usuario",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Por favor ingrese su usuario";
-                    }
-                    return null;
-                  },
-                ),
-                //campo en el cual el usuario va a ingresar el grado que cursa
-                const SizedBox(height: 20),
-                const Text("Ingrese el grado que cursa",
-                    style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  key: const ValueKey("Grado"),
-                  controller: _grade,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Grado",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Por favor ingrese el grado que cursa";
-                    }
-                    return null;
-                  },
-                ),
-                //campo en el cual el usuario va a ingresar su colegio
-                const SizedBox(height: 20),
-                const Text("Ingrese su colegio",
-                    style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  key: const ValueKey("Colegio"),
-                  controller: _school,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Colegio",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Por favor ingrese su colegio";
-                    }
-                    return null;
-                  },
-                ),
-                //campo en el cual el usuario va a ingresar la fecha de nacimiento
-                const SizedBox(height: 20),
-                // usaremos 3 campos de texto para ingresar la fecha de nacimiento ubicados en una fila
-                const Text("Ingrese su fecha de nacimiento",
-                    style: TextStyle(fontSize: 20)),
-                Row(
-                  children: [
-                    Expanded(
-                      child: TextFormField(
-                        key: const ValueKey("Dia"),
-                        controller: _birthDay,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Dia",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Por favor ingrese el dia";
-                          }
-                          return null;
-                        },
-                      ),
+            //colocar scroll en el formulario
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  //campo en el cual el usuario va a ingresar su usuario
+                  const Text("Ingrese su usuario",
+                      style: TextStyle(fontSize: 20)),
+                  TextFormField(
+                    key: const ValueKey("UsuarioRegistro"),
+                    controller: _user,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Usuario",
                     ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: TextFormField(
-                        key: const ValueKey("Mes"),
-                        controller: _birthMonth,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Mes",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Por favor ingrese el mes";
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                    const SizedBox(width: 10),
-                    Expanded(
-                      child: TextFormField(
-                        key: const ValueKey("Año"),
-                        controller: _birthYear,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Año",
-                        ),
-                        validator: (value) {
-                          if (value!.isEmpty) {
-                            return "Por favor ingrese el año";
-                          }
-                          return null;
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-
-                //campo en el cual el usuario va a ingresar su contraseña
-                const SizedBox(height: 20),
-                const Text("Ingrese su contraseña",
-                    style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  key: const ValueKey("Contraseña1"),
-                  controller: _password1,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Contraseña",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Por favor ingrese su contraseña";
-                    }
-                    if (value.length < 8) {
-                      return "La contraseña debe tener al menos 8 caracteres";
-                    }
-                    return null;
-                  },
-                ),
-                //campo en el cual el usuario va a ingresar su contraseña nuevamente
-                const SizedBox(height: 20),
-                const Text("Ingrese su contraseña nuevamente",
-                    style: TextStyle(fontSize: 20)),
-                TextFormField(
-                  key: const ValueKey("Contraseña2"),
-                  controller: _password2,
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: "Contraseña",
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return "Por favor ingrese su contraseña";
-                    }
-                    if (value.length < 8) {
-                      return "La contraseña debe tener al menos 8 caracteres";
-                    }
-                    if (value != _password1.text) {
-                      return "Las contraseñas no coinciden";
-                    }
-                    return null;
-                  },
-                ),
-
-                // Boton de inicio de seccion
-                const SizedBox(height: 10),
-                OutlinedButton(
-                    key: const ValueKey("Registrar"),
-                    onPressed: () {
-                      if (_formulario.currentState!.validate()) {
-                        Get.snackbar("Felicidades", "Registro exitoso");
-                        authController
-                            .register(
-                                birthDay: int.parse(_birthDay.text),
-                                birthMonth: int.parse(_birthMonth.text),
-                                birthYear: int.parse(_birthYear.text),
-                                grade: int.parse(_grade.text),
-                                school: _school.text,
-                                username: _user.text,
-                                password: _password1.text)
-                            .then((value) {
-                          if (value) {
-                            Get.offAllNamed('/');
-                          }
-                        });
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Por favor ingrese su usuario";
                       }
+                      return null;
                     },
-                    child: const Text("Registrarse")),
-              ],
+                  ),
+                  // campos nombre y apellido
+                  const SizedBox(height: 20),
+                  const Text("Ingrese su nombre y apellido",
+                      style: TextStyle(fontSize: 20)),
+                  Row(
+                    // usaremos 2 campos de texto para ingresar el nombre y apellido ubicados en una fila
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          key: const ValueKey("Nombre"),
+                          controller: _user,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Nombre",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Por favor ingrese su nombre";
+                            }
+                            //si tiene numeros o caracteres especiales
+                            if (value.contains(RegExp(r'[0-9]')) ||
+                                value.contains(
+                                    RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+                              return "El nombre no puede contener numeros o caracteres especiales";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: TextFormField(
+                          key: const ValueKey("Apellido"),
+                          controller: _user,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Apellido",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Por favor ingrese su apellido";
+                            }
+                            if (value.contains(RegExp(r'[0-9]')) ||
+                                value.contains(
+                                    RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
+                              return "El apellido no puede contener numeros o caracteres especiales";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  //campo en el cual el usuario va a ingresar el grado que cursa
+                  const SizedBox(height: 20),
+                  const Text("Ingrese el grado que cursa",
+                      style: TextStyle(fontSize: 20)),
+                  TextFormField(
+                    key: const ValueKey("Grado"),
+                    controller: _grade,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Grado",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Por favor ingrese el grado que cursa";
+                      }
+                      return null;
+                    },
+                  ),
+                  //campo en el cual el usuario va a ingresar su colegio
+                  const SizedBox(height: 20),
+                  const Text("Ingrese su colegio",
+                      style: TextStyle(fontSize: 20)),
+                  TextFormField(
+                    key: const ValueKey("Colegio"),
+                    controller: _school,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Colegio",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Por favor ingrese su colegio";
+                      }
+                      return null;
+                    },
+                  ),
+                  //campo en el cual el usuario va a ingresar la fecha de nacimiento
+                  const SizedBox(height: 20),
+                  // usaremos 3 campos de texto para ingresar la fecha de nacimiento ubicados en una fila
+                  const Text("Ingrese su fecha de nacimiento",
+                      style: TextStyle(fontSize: 20)),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextFormField(
+                          key: const ValueKey("Dia"),
+                          controller: _birthDay,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Dia",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Por favor ingrese el dia";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: TextFormField(
+                          key: const ValueKey("Mes"),
+                          controller: _birthMonth,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Mes",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Por favor ingrese el mes";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Expanded(
+                        child: TextFormField(
+                          key: const ValueKey("Año"),
+                          controller: _birthYear,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Año",
+                          ),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return "Por favor ingrese el año";
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //campo en el cual el usuario va a ingresar su contraseña
+                  const SizedBox(height: 20),
+                  const Text("Ingrese su contraseña",
+                      style: TextStyle(fontSize: 20)),
+                  TextFormField(
+                    key: const ValueKey("Contraseña1"),
+                    controller: _password1,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Contraseña",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Por favor ingrese su contraseña";
+                      }
+                      if (value.length < 8) {
+                        return "La contraseña debe tener al menos 8 caracteres";
+                      }
+                      return null;
+                    },
+                  ),
+                  //campo en el cual el usuario va a ingresar su contraseña nuevamente
+                  const SizedBox(height: 20),
+                  const Text("Ingrese su contraseña nuevamente",
+                      style: TextStyle(fontSize: 20)),
+                  TextFormField(
+                    key: const ValueKey("Contraseña2"),
+                    controller: _password2,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Contraseña",
+                    ),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Por favor ingrese su contraseña";
+                      }
+                      if (value.length < 8) {
+                        return "La contraseña debe tener al menos 8 caracteres";
+                      }
+                      if (value != _password1.text) {
+                        return "Las contraseñas no coinciden";
+                      }
+                      return null;
+                    },
+                  ),
+
+                  // Boton de inicio de seccion
+                  const SizedBox(height: 10),
+                  OutlinedButton(
+                      key: const ValueKey("Registrar"),
+                      onPressed: () {
+                        if (_formulario.currentState!.validate()) {
+                          Get.snackbar("Felicidades", "Registro exitoso");
+                          authController
+                              .register(
+                                  birthDay: int.parse(_birthDay.text),
+                                  birthMonth: int.parse(_birthMonth.text),
+                                  birthYear: int.parse(_birthYear.text),
+                                  grade: int.parse(_grade.text),
+                                  school: _school.text,
+                                  username: _user.text,
+                                  password: _password1.text)
+                              .then((value) {
+                            if (value) {
+                              Get.offAllNamed('/');
+                            }
+                          });
+                        }
+                      },
+                      child: const Text("Registrarse")),
+                ],
+              ),
             ),
           ),
           // ),
